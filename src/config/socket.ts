be4e24271380a32b62dc8194403e856c828be46c
Nicolas Pereira <nicopv.dev@gmail.com>
2022-05-18 23:48:27 -0400

    agrega VITE_SOCKET_URI .env

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const SOCKET_URL = import.meta.SOCKET_URI || "http://localhost:8000";
+const SOCKET_URL = import.meta.VITE_SOCKET_URI || "http://localhost:8000";
 
 export const socket = io(SOCKET_URL);
