fa2640fd03c98ff1b0427dc661c7cd881ea4aadd
Nicolas Pereira <nicopv.dev@gmail.com>
2022-05-18 23:42:12 -0400

    agregada url socket

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -1,3 +1,5 @@
 import io from "socket.io-client";
 
-export const socket = io("http://localhost:8000");
+const SOCKET_URL = import.meta.SOCKET_URI || "http://localhost:8000";
+
+export const socket = io(SOCKET_URL);
